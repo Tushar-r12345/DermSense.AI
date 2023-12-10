fd47be3fef71e2f67fbf55d0c5698fd64ea968f1
Tushar Pal <90563855+Tushar-r12345@users.noreply.github.com>
2023-12-10 23:23:01 +0530

    Update 4_🔬_Diagnoser.py

--- pages/4_🔬_Diagnoser.py
+++ pages/4_🔬_Diagnoser.py
@@ -17,73 +17,6 @@ css_file = current_dir / "styles" / "main.css"
 # Define the class labels for your model
 classes = ['ekzama', 'healthy', 'pigment', 'vitiligo']
 
-class ConvNet(nn.Module):
-    def __init__(self, num_classes=4):
-        super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(num_features=12)
-        self.relu1 = nn.ReLU()
-        self.pool = nn.MaxPool2d(kernel_size=2)
-        self.conv2 = nn.Conv2d(in_channels=12, out_channels=20, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(num_features=20)
-        self.relu2 = nn.ReLU()
-        self.conv3 = nn.Conv2d(in_channels=20, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(num_features=32)
-        self.relu3 = nn.ReLU()
-        self.fc = nn.Linear(in_features=75 * 75 * 32, out_features=num_classes)
-
-    def forward(self, input):
-        output = self.conv1(input)
-        output = self.bn1(output)
-        output = self.relu1(output)
-        output = self.pool(output)
-        output = self.conv2(output)
-        output = self.bn2(output)
-        output = self.relu2(output)
-        output = self.conv3(output)
-        output = self.bn3(output)
-        output = self.relu3(output)
-        output = output.view(-1, 32 * 75 * 75)
-        output = self.fc(output)
-        return output
-
-# Load the pre-trained PyTorch model
-if torch.cuda.is_available():
-    model = torch.load('best_checkpoint20.model')
-else:
-    model = torch.load('best_checkpoint20.model', map_location=torch.device('cpu'))
-# Check if the model is an OrderedDict
-if isinstance(model, collections.OrderedDict):
-    # If it is an OrderedDict, extract the model parameters
-    model_state_dict = model
-    model = ConvNet()  # Replace ConvNet with your actual model class
-    model.load_state_dict(model_state_dict)
-else:
-    # If it's already a model object, proceed as usual
-    model.eval()
-
-# Function to preprocess the image
-def preprocess_image(image_path):
-    image = Image.open(image_path)
-    image = image.resize((150, 150))
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    image = transform(image).unsqueeze(0)
-    return image
-
-# Function to predict the disease
-def predict_disease(image_path):
-    image = preprocess_image(image_path)
-
-    with torch.no_grad():
-        outputs = model(image)
-        probabilities = torch.nn.functional.softmax(outputs, dim=1)
-        _, predicted = torch.max(outputs.data, 1)
-
-    predicted_class = classes[predicted.item()]
-    return predicted_class, probabilities.numpy()[0]
 
 def main():
     # Set the title and layout of the app
@@ -104,8 +37,6 @@ def main():
         image = image.resize((300, 300))
         st.image(image, caption='Selected Image', use_column_width=True)
 
-        # Predict the disease
-        predicted_class, probabilities = predict_disease(uploaded_file)
 
         # Display the confidence scores in a table
         st.subheader("Confidence Scores")
@@ -122,29 +53,6 @@ def main():
     st.write("Age:", age)
     st.write("Gender:", gender)
     st.write("Blood Group:", bg)
-
-    # Add a "Print as PDF" button
-    if st.button("Print as PDF"):
-        # Create a PDF document
-        pdf_buffer = BytesIO()
-        pdf = PDFDocument(pdf_buffer)
-
-        # Add the information to the PDF document
-        pdf.init_report()
-        pdf.h1("Predicted Disease")
-        pdf.p(predicted_class)
-        pdf.h1("Entered Information")
-        pdf.p(f"Name: {name}")
-        pdf.p(f"Age: {age}")
-        pdf.p(f"Gender: {gender}")
-        pdf.p(f"Blood Group: {bg}")
-        pdf.generate()
-
-        # Save the PDF file
-        pdf_buffer.seek(0)
-        b64_data = base64.b64encode(pdf_buffer.read()).decode()
-        href = f'<a href="data:application/octet-stream;base64,{b64_data}" download="prediction_report.pdf">Download PDF</a>'
-        st.markdown(href, unsafe_allow_html=True)
 
     # Footer text and links
     footer_text = "copyright © 2023, All rights reserved.Created by Tushar Pal (iam#11) ! This is prototype only"
